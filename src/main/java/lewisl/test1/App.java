5efe81f9a29804aae1fabda41a56f07a121b0986
lewisl <yi_liu@tcl.com>
2014-07-17 08:38:28 +0800

    xxxxx

--- src/main/java/lewisl/test1/App.java
+++ src/main/java/lewisl/test1/App.java
@@ -2,12 +2,12 @@ package lewisl.test1;
 
 /**
  * Hello world!
- *
  */
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-                 //comments by lewisl
+		// comments by lewisl
 		System.out.println("Hello Lewisl!");
 	}
+
 }
