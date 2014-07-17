fadfef7218fd60d6bc6fb2f34cc578109f737eab
lewisl <yi_liu@tcl.com>
2014-07-17 09:57:13 +0800

    add chinese words

--- src/main/java/lewisl/test1/App.java
+++ src/main/java/lewisl/test1/App.java
@@ -1,14 +1,14 @@
 package lewisl.test1;
 
 /**
- * Hello world!
+ * Hello world! 中文显示测试
  */
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		// comments by lewisl
 		System.out.println("Hello Lewisl!");
-		
+
 		System.out.println("The font size...");
 	}
 
