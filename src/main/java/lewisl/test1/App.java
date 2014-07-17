afa42faceba062f81e1e4d896253cf4ab6570445
lewisl <yi_liu@tcl.com>
2014-07-17 09:57:13 +0800

    add chinese words
    Change-Id: I838e6d48d273deaf24176ed47882de404e4d2277
    
    Signed-off-by: lewisl <yi_liu@tcl.com>

--- src/main/java/lewisl/test1/App.java
+++ src/main/java/lewisl/test1/App.java
@@ -1,15 +1,18 @@
 package lewisl.test1;
 
 /**
- * Hello world!
+ * Hello world! 中文显示测试
  */
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		// comments by lewisl
 		System.out.println("Hello Lewisl!");
-		
+
 		System.out.println("The font size...");
+		
+		System.out.println("I am very glad to join TCL...");
+		
 	}
 
 }
